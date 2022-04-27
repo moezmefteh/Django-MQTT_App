68d82be6f84c27dbbb2b6fc89aa6db2039cdab98
moezmefteh85@gmail.com <moezmefteh85@gmail.com>
2022-04-27 02:15:33 +0100

    adding models

--- MqttApp/admin.py
+++ MqttApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import motor, presion, msg,action
+
+admin.site.register(action)
+admin.site.register(presion)
+admin.site.register(msg)
+
+admin.site.register(motor)
--- MqttApp/models.py
+++ MqttApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class presion(models.Model):
+    pub_date = models.DateTimeField('date published')
+    value = models.CharField(max_length=200)
+
+class msg(models.Model):
+    pub_date = models.DateTimeField('date published')
+    value = models.CharField(max_length=200)
+
+class action(models.Model):
+    pub_date = models.DateTimeField('date published')
+    value = models.CharField(max_length=200)
+
+
+class motor(models.Model):
+    pub_date = models.DateTimeField('date published')
+    value = models.CharField(max_length=200)

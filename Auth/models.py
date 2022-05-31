e65690bb7e420e9d4f5c7e3f3080517f58bb100a
moez mefteh <moezmefteh85@gmail.com>
2022-05-31 15:05:58 +0100

    editing expiration time

--- Auth/models.py
+++ Auth/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name= models.CharField(max_length=30)
     last_name= models.CharField(max_length=30)
-    cin     = models.IntegerField(default=00000000)
+    cin     = models.CharField(max_length=30)
     username= models.CharField(max_length=30,primary_key=True)   
     password= models.CharField(max_length=255)
     poste   = models.CharField(max_length=30,default="technicien")
     email   = models.EmailField()
-    telephone= models.IntegerField(default=00000000)
+    telephone= models.CharField(max_length=30)
     is_superuser=models.BooleanField(default=False)
     is_staff=models.BooleanField(default=False)
 
